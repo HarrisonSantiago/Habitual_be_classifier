abebd8382824cef8d88d788d679d26e22667e272
Harrison <harrisonsantiago4@gmail.com>
2022-03-13 16:36:23 -0400

    Updated rules

--- Habitual_be_classifier/rules.py
+++ Habitual_be_classifier/rules.py
@@ -1,4 +1,4 @@
-from nltk import *
+import nltk
 import numpy as np
 
 
@@ -16,15 +16,12 @@ def rule_filter(dataset):
 
     declared_nonhab = []
     unknown = []
-    ### Before using this file, modify Parameters.py to fit desried values ###
 
     for row in dataset:
-        # This takes all instances of ' be ' and the next 25 characters
-        # if the next 25 characters have -ing, then it is habitual
 
-        tokenizer = RegexpTokenizer(r"\w+")
-        tokenized = tokenizer.tokenize(row[0])
-        token_list = pos_tag(tokenized)
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        tokenized = nltk.tokenizer.tokenize(row[0])
+        token_list = nltk.pos_tag(tokenized)
 
         i = row[1].astype(np.int)
         if (token_list[i][0] != 'be'):
@@ -37,8 +34,6 @@ def rule_filter(dataset):
             elif(token_list[i+1][0] == 'be'):
                 i = i+1
 
-
-
         if (token_list[i][0] != 'be'):
             continue
 
@@ -46,17 +41,13 @@ def rule_filter(dataset):
         if token_list[i - 1][1] == "MD" or token_list[i - 1][1] == "TO" or token_list[i - 1][1] == "JJ":
             declared_nonhab.append([row[0], i, row[2]])
 
-
-        # Good for 0s
         elif token_list[i + 1][1] == "VBN" and token_list[i - 1][1] != "PRP" and token_list[i - 1][1] != "NN":
             declared_nonhab.append([row[0], i, row[2]])
 
-        # This returned a majority 0, this goes to advective
         elif (token_list[i + 1][1] == "JJ" and token_list[i - 1][1] != "PRP" and token_list[i - 1][1] != "NN" and
              token_list[i - 1][1] != "NNS" and token_list[i - 1][1] != "RB"):
             declared_nonhab.append([row[0], i, row[2]])
 
-        # All 0s, prepositional phrases correspond to IN
         elif token_list[i + 1][1] == "IN" and (
                 token_list[i - 1][1] == "TO" or token_list[i - 1][1] == "MD" or token_list[i - 1][1] == "VBP" or
                 token_list[i - 1][1] == "VBZ"):
@@ -69,7 +60,6 @@ def rule_filter(dataset):
 
         elif (token_list[i - 1][1] == "NN" and token_list[i - 2][1] == "JJ"):
             declared_nonhab.append([row[0], i, row[2]])
-
 
         else:
             unknown.append([row[0], i, row[2]])
