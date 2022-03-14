33daa63e61b5c58576481c47897158ea22e5203a
Harrison <harrisonsantiago4@gmail.com>
2022-03-13 20:25:06 -0400

    Updated augmenter

--- Habitual_be_classifier/__init__.py
+++ Habitual_be_classifier/__init__.py
@@ -1,6 +1,6 @@
 from .Augmenter import findOccurrences, augmenter
 from .Load_pretrained import get_pretrained
 from .Train import algo_trainers
-from .get_classifiers import processer, get_classifiers
+from .get_classifiers import vectorize, get_classifiers
 from .rules import rule_filter
 from .csv_processor import csv_processor
