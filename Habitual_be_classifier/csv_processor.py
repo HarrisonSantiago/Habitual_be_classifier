d3c26850bccce272edc4485deba57827e32dbf0d
Harrison <harrisonsantiago4@gmail.com>
2022-04-16 14:58:45 -0400

    csv_processor

--- Habitual_be_classifier/csv_processor.py
+++ Habitual_be_classifier/csv_processor.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import re
 
 def findOccurrences(s, ch):
-    return [i for i, letter in enumerate(s) if letter == ch]
+    return [i for i in range(len(s)) if s.startswith(ch, i)]
+    #return [i for i, letter in enumerate(s) if letter == ch]
 
 def csv_processor(filePath):
 
@@ -23,22 +25,29 @@ def csv_processor(filePath):
 
         for index, row in text.iterrows():
             input_row = str(text.iloc[index,1]).lower()
-            spaces = findOccurrences(input_row, ' ')
+            be_indices = findOccurrences(input_row, 'be')
 
-            assert 48 in spaces or 49 in spaces, 'location of be not where expected'
+            #assert 48 in spaces or 49 in spaces, 'location of be not where expected'
 
-            be_index = spaces.index(48) if 48 in spaces else spaces.index(49)
+            diff = np.absolute(be_indices - 50)
+            be_index = diff.argmin()
+            #be_index = spaces.index(48) if 48 in spaces else spaces.index(49)
 
-            if len(spaces) - be_index > 5:
+            if len(be_indices) - be_index > 5:
                 input_row = " " + input_row + " "
                 input_row = re.sub("[^\w\s']", "", input_row) # remove punctuation
                 input_row = input_row.replace(" be'", " be ")
                 hab = int(text.iloc[index, 0])
 
+                be_indices = findOccurrences(input_row, 'be')
+                diff = np.absolute(be_indices - 50)
+                be_index = diff.argmin()
+
+                word_index = input_row.count(' ', 0, be_index)
                 if hab == 1:
-                    hab_lst.append([input_row, be_index+1, 1])
+                    hab_lst.append([input_row, word_index+1, 1])
                 if hab == 0:
-                    nonhab_lst.append([input_row, be_index+1, 0])
+                    nonhab_lst.append([input_row, word_index+1, 0])
 
 
 
