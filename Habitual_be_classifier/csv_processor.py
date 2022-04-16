ffd484d9137e22a0d447e2d4f4f6c96aceb9fc4e
Harrison <harrisonsantiago4@gmail.com>
2022-04-16 15:18:27 -0400

    csv

--- Habitual_be_classifier/csv_processor.py
+++ Habitual_be_classifier/csv_processor.py
@@ -41,8 +41,8 @@ def csv_processor(filePath):
                 input_row = input_row.replace(" be'", " be ")
                 hab = int(text.iloc[index, 0])
 
-                be_indices = findOccurrences(input_row, 'be')
-                diff = np.absolute(be_indices - 50)
+                be_indices = findOccurrences(input_row, ' be ')
+                diff = np.absolute(np.array(be_indices) - 50)
                 be_index = diff.argmin()
 
                 word_index = input_row.count(' ', 0, be_index)
